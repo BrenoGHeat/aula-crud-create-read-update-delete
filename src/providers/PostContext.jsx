c20c813753a555fe20d0897ed6d1d4d2f90fc3ee
Breno Garcia <BrenoGHeat@gmail.com>
2024-07-24 14:44:51 -0300

    atividade realizada

--- src/providers/PostContext.jsx
+++ src/providers/PostContext.jsx
@@ -6,99 +6,97 @@ import { api } from "../services/api";
 export const PostContext = createContext({});
 
 export const PostProvider = ({ children }) => {
-   const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
-   const [postList, setPostList] = useState([]);
-   const [editingPost, setEditingPost] = useState(null);
+  const [postList, setPostList] = useState([]);
 
-   console.log(editingPost);
+  const [editingPost, setEditingPost] = useState(null);
 
-   useEffect(() => {
-      const getPosts = async () => {
-         try {
-            const { data } = await api.get("/news");
-            setPostList(data);
-         } catch (error) {
-            console.log(error);
-         }
-      };
-      getPosts();
-   }, []);
-
-   const postCreate = async (formData) => {
+  useEffect(() => {
+    const getPosts = async () => {
       try {
-         const newPost = { ...formData, author: user.name };
-
-         const token = localStorage.getItem("@TOKEN");
-
-         const { data } = await api.post("/news", newPost, {
-            headers: {
-               Authorization: `Bearer ${token}`,
-            },
-         });
-
-         setPostList([...postList, data]);
+        const { data } = await api.get("/news");
+        setPostList(data);
       } catch (error) {
-         console.log(error);
+        console.log(error);
       }
-   };
+    };
+    getPosts();
+  }, []);
 
-   const postUpdate = async (formData) => {
-      try {
-         const token = localStorage.getItem("@TOKEN");
+  const postCreate = async (formData) => {
+    try {
+      const newPost = { ...formData, author: user.name };
 
-         const { data } = await api.patch(`/news/${editingPost.id}`, formData, {
-            headers: {
-               Authorization: `Bearer ${token}`,
-            },
-         });
+      const token = localStorage.getItem("@TOKEN");
 
-         const newPostList = postList.map((post) => {
-            if (post.id === editingPost.id) {
-               return data;
-            } else {
-               return post;
-            }
-         });
+      const { data } = await api.post("/news", newPost, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setPostList([...postList, data]);
+    } catch (error) {
+      console.log();
+    }
+  };
 
-         setPostList(newPostList);
-         setEditingPost(null);
-      } catch (error) {
-         console.log(error);
-      }
-   };
+  const postUpdate = async (formData) => {
+    try {
+      const token = localStorage.getItem("@TOKEN");
 
-   const postDelete = async (deletingId) => {
-      try {
-         const token = localStorage.getItem("@TOKEN");
+      const { data } = await api.patch(`/news/${editingPost.id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
-         await api.delete(`/news/${deletingId}`, {
-            headers: {
-               Authorization: `Bearer ${token}`,
-            },
-         });
+      const newPostList = postList.map((post) => {
+        if (post.id === editingPost.id) {
+          return data;
+        } else {
+          return post;
+        }
+      });
 
-         const newPostList = postList.filter((post) => post.id !== deletingId);
-         setPostList(newPostList);
+      setPostList(newPostList);
+      setEditingPost(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-         alert("Exclusão realizada com sucesso!");
-      } catch (error) {
-         console.log(error);
-      }
-   };
+  const postDelete = async (deletingId) => {
+    try {
+      const token = localStorage.getItem("@TOKEN");
 
-   return (
-      <PostContext.Provider
-         value={{
-            postCreate,
-            postUpdate,
-            postDelete,
-            postList,
-            editingPost,
-            setEditingPost,
-         }}
-      >
-         {children}
-      </PostContext.Provider>
-   );
+      await api.delete(`/news/${deletingId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const newPostList = postList.filter((post) => post.id !== deletingId);
+      setPostList(newPostList);
+
+      alert("Exclusão realizada com sucesso!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <PostContext.Provider
+      value={{
+        postCreate,
+        postUpdate,
+        postDelete,
+        postList,
+        editingPost,
+        setEditingPost,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
 };
