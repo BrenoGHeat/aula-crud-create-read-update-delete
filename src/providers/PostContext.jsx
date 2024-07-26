d735f56468e29462480d13d2d00e4ba103ef83ef
Breno Garcia <BrenoGHeat@gmail.com>
2024-07-26 12:50:25 -0300

    react query atividade feita

--- src/providers/PostContext.jsx
+++ src/providers/PostContext.jsx
@@ -2,45 +2,60 @@ import { useContext, useEffect } from "react";
 import { createContext, useState } from "react";
 import { UserContext } from "./UserContext";
 import { api } from "../services/api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const PostContext = createContext({});
 
 export const PostProvider = ({ children }) => {
   const { user } = useContext(UserContext);
+  const [ editingPost, setEditingPost] = useState(null);
 
-  const [postList, setPostList] = useState([]);
 
-  const [editingPost, setEditingPost] = useState(null);
+  const { data: postList } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      const { data } = await api.get("/news");
+      return data;
+    },
+  });
 
-  useEffect(() => {
-    const getPosts = async () => {
-      try {
-        const { data } = await api.get("/news");
-        setPostList(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getPosts();
-  }, []);
+  const client = useQueryClient();
 
-  const postCreate = async (formData) => {
-    try {
+  const revalidate = () => {
+    client.invalidateQueries({ queryKey: ["posts"] });
+  };
+
+  const postCreate = useMutation({
+    mutationFn: async (formData) => {
       const newPost = { ...formData, author: user.name };
 
       const token = localStorage.getItem("@TOKEN");
-
-      const { data } = await api.post("/news", newPost, {
+      return await api.post("/news", newPost, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-      setPostList([...postList, data]);
-    } catch (error) {
-      console.log();
-    }
-  };
+    },
+    onSuccess: revalidate,
+  });
 
+  const postUpdate = useMutation({
+    mutationFn: async (formData) => {
+      const token = localStorage.getItem("@TOKEN");
+
+      return await api.patch(`/news/${editingPost.id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess: () => {
+      setEditingPost(null);
+      revalidate();
+    },
+  });
+
+  /*
   const postUpdate = async (formData) => {
     try {
       const token = localStorage.getItem("@TOKEN");
@@ -65,7 +80,25 @@ export const PostProvider = ({ children }) => {
       console.log(error);
     }
   };
+  */
 
+  const postDelete = useMutation({
+    mutationFn: async (deletingId) => {
+      const token = localStorage.getItem("@TOKEN");
+
+      return await api.delete(`/news/${deletingId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess: () => {
+      alert("Exclusão realizada com sucesso!");
+      revalidate();
+    },
+  });
+
+  /*
   const postDelete = async (deletingId) => {
     try {
       const token = localStorage.getItem("@TOKEN");
@@ -84,6 +117,7 @@ export const PostProvider = ({ children }) => {
       console.log(error);
     }
   };
+  */
 
   return (
     <PostContext.Provider
